6643dcecd770c3373f97cd03f74f22cfb9f48c3d
Dreiz0x <cisne170990@gmail.com>
2026-04-12 01:28:38 -0600

    Fix formatting of Room dependency in build.gradle.kts

--- app/src/main/java/com/anekon/ci/domain/usecase/ProjectGeneratorUseCase.kt
+++ app/src/main/java/com/anekon/ci/domain/usecase/ProjectGeneratorUseCase.kt
@@ -77,7 +77,7 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
-    ${if (config.useRoom) "id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false" else ""}
+    ${if (config.useRoom) "id(\"com.google.devtools.ksp\") version \"1.9.22-1.0.17\" apply false" else ""}
 }
 """
         File(baseDir, "build.gradle.kts").writeText(content)
